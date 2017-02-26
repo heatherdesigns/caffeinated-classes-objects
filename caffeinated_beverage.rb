604ce2cce334ef89556be1fab50f36daea389c63
Heather Taylor <heather@heysparkbox.com>
2017-02-26 16:06:27 -0500

    feat: add comments
    Add comments after feedback with Yock with the goal of taking action in the coming days.

--- caffeinated_beverage.rb
+++ caffeinated_beverage.rb
@@ -2,7 +2,7 @@ class Caffeinated_Beverage
   
   attr_accessor :num_ounces
   
-  def initialize(num_ounces)
+  def initialize(num_ounces) # idea from Yock: instantiate concentration of caffeine & label of beverage // served hot & served cold
     # Code School used self.num_ounces instead of @num_ounces. Is one better than the other?
     @num_ounces = num_ounces
   end 
@@ -11,7 +11,7 @@ class Caffeinated_Beverage
     num_ounces *= 10.75
   end 
   
-  def to_s
+  def to_s # need to change
     puts "The average #{num_ounces} ounce caffeinated beverage has #{caffeinate(num_ounces)} mg of caffeine."
   end 
   
