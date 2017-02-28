3d75be1e5773e05c4d08b712bc6fb018b9538de5
Heather Taylor <heather@heysparkbox.com>
2017-02-28 09:48:04 -0500

    docs: add comments
    Add comments to reflect thought process. These comments will be removed as code is written.

--- coffee_shop.rb
+++ coffee_shop.rb
@@ -8,4 +8,18 @@ require_relative "./soda.rb"
 
 class CoffeeShop
   
+  # need class for orders?
+  # take customer's order - coffee, tea or soda (combine these three classes into the caffeinated beverage class - see notes on Yock's suggestion.)
+  # need size
+  # add espresso - # of shots
+  # add milk - def milk or class Milk?
+  # add flavoring - (this can apply to soda too! think cherry, vanilla or chocolate coke) def flavoring or class Flavoring? 
+  # caffeine content is calculated and saved for future reference
+  # get customer name
+  # internal order id is created
+  # order pushes onto the queue (an array that stores customer orders)
+  # order is made.......... Perhaps a statement prints processing order # for customer XYZ.
+  # customer is alerted that their order is ready, pops off the queue
+  # customer receives snarky comment regarding their caffeinated beverage based upon the amount of caffeine
+  
 end
