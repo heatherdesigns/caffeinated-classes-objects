ddd9ddd1102ab076d519251e054ec609b042b1af
Heather Taylor <heather@heysparkbox.com>
2017-03-06 14:01:23 -0500

    feat: update caffeinate function
    Updated caffeinate function to take in a beverage parameter that is used to determine the caffeine per ounce.

--- caffeinated_beverage.rb
+++ caffeinated_beverage.rb
@@ -1,16 +1,24 @@
 class Caffeinated_Beverage
   
-  attr_accessor :beverage, :num_ounces, :caffeine_per_oz
+  attr_accessor :beverage, :num_ounces
   
-  def initialize(beverage, num_ounces, caffeine_per_oz) 
+  def initialize(beverage, num_ounces) 
     @beverage = beverage
     @num_ounces = num_ounces
-    @caffeine_per_oz = caffeine_per_oz
   end 
   
-  def caffeinate(num_ounces)
+  def caffeinate(beverage, num_ounces)
+    if beverage == "coffee" 
+      caffeine_per_oz = 18.0
+    elsif beverage == "tea"
+      caffeine_per_oz = 3.5
+    elsif beverage == "soda"
+      caffeine_per_oz = 3.75 
+    else
+      caffeine_per_oz = 0.0
+    end
     num_ounces *= caffeine_per_oz
-  end 
+  end
   
   def output
     puts "The average #{num_ounces} ounce #{beverage} has #{caffeinate(num_ounces)} mg of caffeine."
